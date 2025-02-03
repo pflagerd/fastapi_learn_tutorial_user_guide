f82599631f974eaac41b49e174fee2783e312e8c
Daniel P Pflager <daniel.pflager@gmail.com>
2025-02-02 19:36:49 -0800

    progress

--- first_steps/tutorial001.py/setmeup.py
+++ first_steps/tutorial001.py/setmeup.py
@@ -1,4 +1,1 @@
-import os
-
-# Fake the filename to make it appear local
-exec(open(os.path.abspath(os.path.join(os.getcwd(), "../../setmeup.py"))).read(), {'__file__': 'setmeup.py', '__name__': '__main__'})
+../../setmeup.py
