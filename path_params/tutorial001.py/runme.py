ce57ead1bb5bdfc5b0006ad13d3f00c9e3ed8c38
Daniel P Pflager <daniel.pflager@gmail.com>
2025-02-02 20:31:51 -0800

    progress

--- path_params/tutorial001.py/runme.py
+++ path_params/tutorial001.py/runme.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 import os
+import setmeup
 import subprocess
 import sys
 
-import setup
 def main(args, debug=False):
-    setup.main([])
-
-    import tempfile
-    user_data_dir=tempfile.mktemp()
+    setmeup.main([])
 
     launcher = "chromium-browser"
-    if not setup.spawn('which chromium-browser').returncode:
-        print(f"user_data_dir == \"{user_data_dir}\"")
-        subprocess.Popen([launcher, "--user-data-dir=/tmp/" + user_data_dir, "--new-window", "http://127.0.0.1:8000", "http://127.0.0.1:8000/docs", "http://127.0.0.1:8000/redoc", "http://127.0.0.1:8000/openapi.json"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if not setmeup.spawn('which chromium-browser').returncode:
+        subprocess.Popen([launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
-        if not setup.spawn('which open').returncode:
+        if not setmeup.spawn('which open').returncode:
             launcher = "open"
-        elif not setup.spawn('which gio').returncode:
+        elif not setmeup.spawn('which gio').returncode:
             launcher = "gio"
         else:
             print('Neither chromium-browser, open nor gio is available in your PATH, cannot launch default browser', file=sys.stderr)
 
-        subprocess.Popen([launcher, "http://127.0.0.1:8000"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        for tab in tabs:
+            subprocess.Popen([launcher, tab], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    os.execvp(".venv/bin/fastapi", [".venv/bin/fastapi", "dev", "main.py"])
+    if os.path.exists("./main.py"):
+        os.execvp(".venv/bin/fastapi", [".venv/bin/fastapi", "dev", "main.py"])
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
