cc5493b458da54ad167e3d44b6fc17e98e09f3b8
Daniel P Pflager <daniel.pflager@gmail.com>
2025-02-10 10:24:18 -0800

    progress

--- runme.py
+++ runme.py
@@ -9,7 +9,9 @@ def main(args, debug=False):
 
     launcher = "chromium-browser"
     if not setmeup.spawn('which chromium-browser').returncode:
-        subprocess.Popen([launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # subprocess.Popen([launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not os.path.exists("./main.py"):
+            os.execvp(launcher, [launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:])
     else:
         if not setmeup.spawn('which open').returncode:
             launcher = "open"
