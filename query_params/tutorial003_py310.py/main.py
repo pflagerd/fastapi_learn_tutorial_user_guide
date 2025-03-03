d322bffd2510daf0c39fa395d091c3b79467a6a6
Daniel P Pflager <daniel.pflager@gmail.com>
2025-03-03 09:11:10 -0800

    progress

---
+++ query_params/tutorial003_py310.py/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    return {"item_id": item_id, "q": q, "short": short}
