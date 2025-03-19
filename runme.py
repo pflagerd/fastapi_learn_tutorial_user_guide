a2debc34dcdf8dcba500d4450b92f58b15e503d1
Daniel P Pflager <daniel.pflager@gmail.com>
2025-03-19 08:54:05 -0700

    progress

--- runme.py
+++ runme.py
@@ -11,8 +11,11 @@ def main(args, debug=False):
 
     if os.path.exists("./main.py"):
         subprocess.Popen([launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        os.execvp(".venv/bin/fastapi", [".venv/bin/fastapi", "dev", "main.py"])
     else:
         os.execvp(launcher, [launcher, "--user-data-dir=" + os.getcwd() + "/.chromium-browser", "--new-window"] + args[1:])
+
+
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
 
